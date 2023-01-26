94c6c9c1498f9032d734c95572df3d7350ba657f
kuresti <ure17002@byui.edu>
2023-01-26 15:56:21 -0700

    chamber

---
+++ chamber/scripts/menu.js
@@ -0,0 +1,6 @@
+const mainnav =
+document.querySelector('.navigation')
+const hambutton = document.querySelector('menu');
+
+hambutton.addEventListener('click', () =>
+{mainnav.classList.toggle('responsive')}, false);
