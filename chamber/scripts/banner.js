e3e4bc0d9adb1644b77ef066f5c6bbfcf4db38e6
kuresti <ure17002@byui.edu>
2023-02-04 08:15:25 -0700

    banner

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,10 @@
+const today = new Date();
+
+const dayNumber = today.getDay();
+
+const element = document.querySelector(".announcement");
+if (dayNumber == 1) {
+    element.classList.add("showme");
+} else if (dayNumber == 2) {
+    element.classList.add("showme");
+} else 
