8b1ec289e6859517f25055a81a62c5516b291a74
kuresti <ure17002@byui.edu>
2023-02-22 11:27:42 -0700

    discover

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,6 +1,7 @@
 //define and get values for variables
 
-const visitsDisplay = document.querySelector("#visits");
+const visitsDisplay = document.querySelector(".visits");
+const nowDisplay =document.querySelector(".now")
 
 //Retrieve the visit value from localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
@@ -12,7 +13,13 @@ if (numVisits !== 0) {
     visitsDisplay.textContent = 'Welcome, this is your first visit!';
 }
 
-//Increase counnt of visits
+//Increase count of visits
 numVisits++;
 //new count is stored in localStorage
 localStorage.setItem("visits-ls", numVisits);
+
+//Days since last visit
+const lastVisit = new Date(document.last-modified).getTime()
+let daysSinceLastVisit = (Date.now() - lastVisit) /84600000;
+let rounded = Math.round(daysSinceLastVisit);
+nowDisplay.textContent = rounded;
