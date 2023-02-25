c30043cc5de1b22b1d4e1813fc954966fc6c66b7
kuresti <ure17002@byui.edu>
2023-02-25 13:14:26 -0700

    join

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,8 +1,11 @@
 const datefield = document.querySelector(".date");
 
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US",
-{ dateStyle: "full"
-}).format(now);
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+
--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -1,3 +1,8 @@
 document.querySelector(
     ".last-modified"
 ).textContent = new Date(document.lastModified);
+
+//Date time for hidden form input
+document.querySelector(
+    ".timeNow"
+).textContent = now.toLocaleString();
