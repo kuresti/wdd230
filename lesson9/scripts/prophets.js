a1b7b673aa157f5ade64f5d32acce9f057fdc486
kuresti <ure17002@byui.edu>
2023-03-02 14:37:08 -0700

    prophets

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -15,33 +15,50 @@ const displayProphets = (prophets) => {
 
   prophets.forEach((prophet) => {
     //create elements to add to the div.cards element
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-    let bDate = document.createElement('p');
-    let bPlace = document.createElement('p');
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let portrait = document.createElement("img");
+    let bDate = document.createElement("p");
+    let bPlace = document.createElement("p");
 
-    //Build the h2 content out to show the prophet's full name - finish the template string
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    // order of prophets
+    let order = prophet.order;
+    let case1 = order % 10;
 
-    //build the p content out to show the prophet's birth place and birth date
-    bDate.textContent = `${prophet.birthdate}`;
-    bPlace.textContent = `${prophet.birthplace}`;
+    if (case1 == 1 && case1 != 11) {
+      let suffix = order + "st";
+    } else if (case1 == 2 && case1 != 12) {
+      let suffix = order + "nd";
+    } else if (case1 == 3 && case1 != 13) {
+      let suffix = order + "rd";
+    } else {
+      let suffix = order + "th";
 
-    //Build the image portrait by setting all the relevant attributes
-    portrait.setAttribute("src", prophet.imageurl);
-    portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
-    portrait.setAttribute("loading", "lazy");
-    portrait.setAttribute("width", "340");
-    portrait.setAttribute("height", "440");
+      //Build the h2 content out to show the prophet's full name - finish the template string
+      h2.textContent = `${prophet.name} ${prophet.lastname}`;
 
-    //Append the section(card) with the created elements
-    card.appendChild(h2);
-    card.appendChild(portrait);
-    card.appendChild(bDate);
-    card.appendChild(bPlace);
+      //build the p content out to show the prophet's birth place and birth date
+      bDate.textContent = `Birth date: ${prophet.birthdate}`;
+      bPlace.textContent = `Birth place: ${prophet.birthplace}`;
 
-    cards.appendChild(card);
+      //Build the image portrait by setting all the relevant attributes
+      portrait.setAttribute("src", prophet.imageurl);
+      portrait.setAttribute(
+        "alt",
+        `Portrait of ${prophet.name} ${prophet.lastname} the ${suffix} prophet`
+      );
+      portrait.setAttribute("loading", "lazy");
+      portrait.setAttribute("width", "250");
+      portrait.setAttribute("height", "300");
+
+      //Append the section(card) with the created elements
+      card.appendChild(h2);
+      card.appendChild(bDate);
+      card.appendChild(bPlace);
+      card.appendChild(portrait);
+
+      cards.appendChild(card);
+    }
   });
 };
 
