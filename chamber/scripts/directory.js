0bf03f9ef92b65a6f5191db8315b9fc20a1a72c5
kuresti <ure17002@byui.edu>
2023-03-04 19:10:44 -0700

    directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -14,3 +14,42 @@ list.addEventListener('click', () => {
     gridShow.classList.add('.list');
 });
 
+// Variable that contains the pathway for directory.js
+const pathway = "data/data.json"
+
+// Asynchronous function to fetch data from jason source
+async function getDirectoryData() {
+    const response = await fetch(pathway);
+    const data = await response.json();
+    //console.table(data.companies);
+    displayCompanies(data.companies);
+  }
+
+//Function to display the json data
+const displayCompanies = (companies) => {
+    const cards = document.querySelector("div.cards");
+
+    //For loop to go through the data in the json file
+    companies.forEach((company) => {
+        //Create elements to add to cards
+        let card = document.createElement("section");
+        let img = document.createElement("img");
+
+    //Build the image portrait by setting all the relevant attributes
+    img.setAttribute("src", company.img);
+    img.setAttribute(
+      "alt",
+      `${company.name}`
+    );
+    img.setAttribute("loading", "lazy");
+    img.setAttribute("width", "200");
+    img.setAttribute("height", "250");
+
+    //Append the section(card) with the created elements
+    card.appendChild(section);
+    card.appendChild(img);
+    });  
+    
+}
+
+  getDirectoryData()
